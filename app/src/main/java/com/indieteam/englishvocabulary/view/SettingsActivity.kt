7bb831fc3ad377902ef7b6604807a3f9ab8a2f2d
mduccc <duc2521997@gmail.com>
2019-08-14 01:51:08 +0700

    Make Google Login

--- app/src/main/java/com/indieteam/englishvocabulary/view/SettingsActivity.kt
+++ app/src/main/java/com/indieteam/englishvocabulary/view/SettingsActivity.kt
@@ -1,20 +1,53 @@
 package com.indieteam.englishvocabulary.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
 import com.indieteam.englishvocabulary.R
 import com.indieteam.englishvocabulary.databinding.ActivitySettingsBinding
 import com.indieteam.englishvocabulary.viewmodel.SettingsViewModel
 
+
 class SettingsActivity : AppCompatActivity() {
 
-    private val settingsViewModel  = SettingsViewModel()
+    companion object{
+        const val googleSignInCode = 10
+    }
+
+    private val settingsViewModel = SettingsViewModel()
+    private lateinit var email: String
+    private fun emailIsInitialized() = ::email.isInitialized
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivitySettingsBinding>(this, R.layout.activity_settings)
         binding.settingsViewModel = settingsViewModel
         binding.executePendingBindings()
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == googleSignInCode && resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val account = task.getResult(ApiException::class.java)
+
+            account?.email?.let {
+                email = it
+            }
+        }
+
+        if (emailIsInitialized()) {
+            Log.d("Email Logged", email)
+        } else {
+            Log.d("Email Logged", "null")
+        }
     }
 }
--- app/src/main/java/com/indieteam/englishvocabulary/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/com/indieteam/englishvocabulary/viewmodel/SettingsViewModel.kt
@@ -1,11 +1,19 @@
 package com.indieteam.englishvocabulary.viewmodel
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.util.Log
+import android.view.View
 import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
+import androidx.databinding.BindingAdapter
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.indieteam.englishvocabulary.BR
 import com.indieteam.englishvocabulary.business.provider.DatabaseManager
 import com.indieteam.englishvocabulary.view.App
+import com.indieteam.englishvocabulary.view.SettingsActivity
 import javax.inject.Inject
 
 class SettingsViewModel : BaseObservable {
@@ -28,6 +36,8 @@ class SettingsViewModel : BaseObservable {
     private var times3 = false
     private var times4 = false
     private var times5 = false
+    private var loginOrLogout = true
+    private var linkTo = ""
 
     @Inject
     lateinit var dataBaseManager: DatabaseManager
@@ -78,6 +88,26 @@ class SettingsViewModel : BaseObservable {
         notifyPropertyChanged(BR.times5)
     }
 
+    @Bindable
+    fun getLoginOrLogout(): Boolean{
+        return loginOrLogout
+    }
+
+    fun setLoginOrLogout(boolean: Boolean) {
+        loginOrLogout = boolean
+        notifyPropertyChanged(BR.loginOrLogout)
+    }
+
+    @Bindable
+    fun getLinkTo(): String {
+        return linkTo
+    }
+
+    fun setLinkTo(string: String) {
+        linkTo = string
+        notifyPropertyChanged(BR.linkTo)
+    }
+
     fun times3Click() {
         setTimes3(true)
         setTimes4(false)
@@ -94,5 +124,45 @@ class SettingsViewModel : BaseObservable {
         setTimes3(false)
         setTimes4(false)
         setTimes5(true)
+    }
+
+    companion object {
+        private var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        @SuppressLint("StaticFieldLeak")
+        private lateinit var mGoogleSignInClient: GoogleSignInClient
+        private lateinit var email: String
+        private fun emailIsInitialized() = ::email.isInitialized
+
+        @BindingAdapter("googleLogin")
+        @JvmStatic
+        fun googleLogin(view: View, loginOrLogout: Boolean) {
+            view.setOnClickListener {
+                if (loginOrLogout) {
+                    // Login
+                    mGoogleSignInClient = GoogleSignIn.getClient(view.context, gso)
+
+                    val account = GoogleSignIn.getLastSignedInAccount(view.context)
+
+                    account?.email?.let {
+                        email = it
+                    }
+
+                    if (emailIsInitialized()) {
+                        Log.d("Email Logged", email)
+                    } else {
+                        Log.d("Email Logged", "null")
+                        val signInIntent = mGoogleSignInClient.signInIntent
+                        (view.context as Activity).startActivityForResult(
+                            signInIntent,
+                            SettingsActivity.googleSignInCode
+                        )
+                    }
+                } else {
+                    // Logout
+                }
+            }
+        }
     }
 }
